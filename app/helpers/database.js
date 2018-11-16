5c414c1dd93eb8e4700ce99a6f26aa3fc65548cd
mmayolini <maxime.mayolini@gmail.com>
2018-11-16 11:09:28 +0100

    ajout mongodb progweb2018

--- app/helpers/database.js
+++ app/helpers/database.js
@@ -1,15 +1,9 @@
-const DABASE_NAME = 'conduiteprojet2018';
-const usersCol  = 'users';
-const projectsCol = 'projets';
-const IDsCol = 'IDs';
-const MONGO_URL = 'mongodb://user:sampleuser1@ds261253.mlab.com:61253/conduiteprojet2018';
+const DABASE_NAME = 'progweb2018';
+const MONGO_URL = 'mongodb://admin1:admin1@ds051658.mlab.com:51658/progweb2018';
 const MongoClient = require('mongodb').MongoClient;
 
 let utils = {
   DBname: DABASE_NAME,
-  usersCol: usersCol,
-  projectsCol: projectsCol,
-  IDsCol: IDsCol,
   DBurl: MONGO_URL,
   MongoClient: MongoClient
 };
