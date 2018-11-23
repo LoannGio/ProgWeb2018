b490c29168c60983b0be9013ed9cbd85895aa8d8
Loann Giovannangeli <loann.giovannangeli@etu.u-bordeaux.fr>
2018-11-23 11:24:02 +0100

    add server

---
+++ api_contract/models/contract.js
@@ -0,0 +1,16 @@
+let DBinfos = require('../helpers/database.js');
+
+// Get projects
+exports.getContracts = async function(startDate, endDate, lowerPrice, higherPrice){
+  const client = await DBinfos.MongoClient.connect(DBinfos.DBurl, {
+      useNewUrlParser: true
+  });
+  const db = client.db(DBinfos.BDname);
+  const filter = {
+    date: {$in: [startDate, endDate]},
+    price: {$in: [lowerPrice, higherPrice]}
+};
+  const contracts = await db.collection(DBinfos.contractCol).find(filter).toArray();
+  client.close();
+  return contracts;
+}
