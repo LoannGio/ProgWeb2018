c429c3acffa20a715f8c3a67c9ae3367c9342a49
mmayolini <maxime.mayolini@gmail.com>
2018-11-30 10:56:31 +0100

    add react

---
+++ app/src/Entreprise.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Entreprise extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Entreprise</h2>
+                <p>Informations POGGEResques en cours de construction</p>
+            </div>
+        );
+    }
+}
+
+export default Entreprise;
