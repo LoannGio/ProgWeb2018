40f40756736121a6f6a0abb9c7b3c180d40b3453
mmayolini <maxime.mayolini@gmail.com>
2018-12-14 10:45:36 +0100

    app rework

--- app/src/index.js
+++ app/src/index.js
@@ -1,13 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./Main";
-import "./stylesheets/bootstrap.css";
-import "./stylesheets/style.css";
+import "./index.css";
 
-const domContainer = document.querySelector('#root');
-ReactDOM.render(e(<Main />), domContainer);
-
-/*ReactDOM.render(
+ReactDOM.render(
     <Main />,
     document.getElementById("root")
-);*/
+);
